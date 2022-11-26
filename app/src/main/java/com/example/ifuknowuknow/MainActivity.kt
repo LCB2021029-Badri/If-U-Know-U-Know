d2f8c933c23bd1ca665cf377649db426c6f5c800
Badri Akkala <lcb2021029@iiitl.ac.in>
2022-11-27 02:03:13 +0530

    idk error during pause

--- app/src/main/java/com/example/ifuknowuknow/MainActivity.kt
+++ app/src/main/java/com/example/ifuknowuknow/MainActivity.kt
@@ -5,10 +5,13 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.MediaController
+import android.widget.VideoView
 import java.io.IOException
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
@@ -16,16 +19,27 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     //
     var sensor : Sensor? = null
     var sensorManager : SensorManager? = null
-    lateinit var display_image : ImageView
+    lateinit var display_video : VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //
-        display_image = findViewById(R.id.display_img)
-        display_image.visibility = View.INVISIBLE
 
+        //image instead of video
+        display_video = findViewById(R.id.video1)
+        display_video.visibility = View.INVISIBLE
+
+        // video instead of image
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(display_video)
+        val offlineUri = Uri.parse("android.resource://$packageName/${R.raw.mvd1}")
+        display_video.setMediaController(mediaController)
+        display_video.setVideoURI(offlineUri)
+        display_video.requestFocus()
+        display_video.start()
+
+        //sensor implementation
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
 
@@ -39,12 +53,16 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         try{
             if(event!!.values[0] < 30 && isRunning == false){ // image is visible when surrounding light is dim i.e. <30
                 isRunning = true
-                display_image.visibility = View.VISIBLE
+                display_video.visibility = View.VISIBLE
+                display_video.resume()
+//                display_video.resume()
             }
             else{   // image is invisible when surroundings is bright
                 isRunning = false
-                display_image.visibility = View.INVISIBLE
+                display_video.visibility = View.INVISIBLE
+                display_video.pause()
 //                display_image.visibility = View.GONE
+//                display_video.pause()
             }
         }
         catch(e : IOException){
@@ -57,11 +75,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
+//        display_video.resume()
         sensorManager!!.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
+//        display_video.pause()
         sensorManager!!.unregisterListener(this)
     }
 }
