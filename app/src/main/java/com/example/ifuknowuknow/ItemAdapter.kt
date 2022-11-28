94fe9dd03fd64d096f45d86c1c5b86cffe5ab2bf
Badri Akkala <lcb2021029@iiitl.ac.in>
2022-11-28 21:08:38 +0530

    i think done

---
+++ app/src/main/java/com/example/ifuknowuknow/ItemAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.ifuknowuknow
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ifuknowuknow.databinding.ItemListBinding
+
+class ItemAdapter(val itemlist: ArrayList<String>):RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    private var binding: ItemListBinding? = null
+
+    inner class ItemViewHolder(itemBinding: ItemListBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        binding = ItemListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemViewHolder(binding!!)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = itemlist[position]
+        holder.itemView.apply {
+            binding?.tvItem?.text = "badri's item $item"
+            if(position%2==0){
+                binding?.tvItem?.setBackgroundColor(ContextCompat.getColor(context,R.color.green))
+            }else{
+                binding?.tvItem?.setBackgroundColor(ContextCompat.getColor(context,R.color.green))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemlist.size
+    }
+
+}
--- app/src/main/java/com/example/ifuknowuknow/MainActivity.kt
+++ app/src/main/java/com/example/ifuknowuknow/MainActivity.kt
@@ -13,19 +13,26 @@ import android.widget.ImageView
 import android.widget.MediaController
 import android.widget.Toast
 import android.widget.VideoView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ifuknowuknow.databinding.ActivityMainBinding
 import java.io.IOException
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    //
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var display_video: VideoView
+
+    //stuff for basic functionality
     var sensor: Sensor? = null
     var sensorManager: SensorManager? = null
-    lateinit var display_video: VideoView
     var mediaController: MediaController? = null
-    lateinit var binding: ActivityMainBinding
     var isRunning: Boolean = true
     var flag = 0
+
+    //stuff for recycler view
+    private val LIST_VIEW = "LIST_VIEW"
+    var currentView = LIST_VIEW
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +56,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         binding.toggleButton.setOnCheckedChangeListener { compoundButton, b ->
             if(b){  //if checked
                 flag = 0
+                binding.notStudying.visibility = View.VISIBLE
                 display_video.visibility = View.VISIBLE
                 display_video.start()
                 binding.studying.visibility = View.GONE
@@ -57,6 +65,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
             else{   //if not checked
                 flag = 1
+                binding.notStudying.visibility = View.GONE
                 display_video.visibility = View.GONE
                 binding.studying.visibility = View.VISIBLE
                 binding.studyMaterial2.visibility = View.GONE
@@ -68,6 +77,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
 
+        //recycker view
+        listView()
+
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -76,6 +88,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             try {
                 if (event!!.values[0] < 30 && isRunning == false && flag == 0) { // image is visible when surrounding light is dim i.e. <30
                     isRunning = true
+                    binding.notStudying.visibility = View.VISIBLE
                     display_video.visibility = View.VISIBLE
                     display_video.start()
                     binding.studying.visibility = View.GONE
@@ -83,6 +96,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                     binding.studyMaterial1.visibility = View.GONE
                 } else {   // image is invisible when surroundings is bright
                     isRunning = false
+                    binding.notStudying.visibility = View.GONE
                     display_video.visibility = View.GONE
                     binding.studying.visibility = View.VISIBLE
                     binding.studyMaterial2.visibility = View.GONE
@@ -107,4 +121,28 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onPause()
         sensorManager!!.unregisterListener(this)
     }
+
+    //working on recycler view
+    private fun getItemList():ArrayList<String>{
+        val list = ArrayList<String>()
+        list.add("Take your first steps programming in Kotlin, add images and text to your Android apps, and learn how to use classes, objects, and conditionals to create an interactive app for your users.")
+        list.add("Build two different apps, and improve the user interface of your app by learning about layouts, Material Design guidelines, and best practices for UI development.")
+        list.add("Enhance your users’ ability to navigate across, into and back out from the various screens within your app for a consistent and predictable user experience.")
+        list.add("Write coroutines for complex code, and learn about HTTP and REST to get data from the internet. Then, use the Coil library to display images in your app.")
+        list.add("Keep your apps working through any disruptions to essential networks or processes for a smooth and consistent user experience.")
+        list.add("Use Android Jetpack’s WorkManager API to schedule necessary background work, like backing up data or downloading fresh content, that keeps running even if the app exits or the device restarts.")
+        list.add("Use Kotlin coroutines to perform multiple tasks at once, and learn about HTTP and REST to get data from the internet using Retrofit. Then use the Coil library to display images in your app.")
+        list.add("Build apps that display a list of data and learn how to make your apps more beautiful with Material Design.")
+        list.add("Continue learning the fundamentals of Kotlin, and start building more interactive apps.Learn the best practices of app architecture to build more complex apps.")
+        list.add("Learn the basics of building Android apps with Jetpack Compose, the new UI toolkit for building Android apps. Along the way, you'll develop a collection of apps to start your journey as an Android developer.")
+
+        return list
+    }
+
+    private fun listView(){
+        currentView = LIST_VIEW
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = ItemAdapter(getItemList())
+    }
+
 }
