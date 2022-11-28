4ac25478793dc8d16b380aeef17750acb44343f4
Badri Akkala <lcb2021029@iiitl.ac.in>
2022-11-28 18:57:05 +0530

    toggle button successful

--- app/src/main/java/com/example/ifuknowuknow/MainActivity.kt
+++ app/src/main/java/com/example/ifuknowuknow/MainActivity.kt
@@ -11,6 +11,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 import com.example.ifuknowuknow.databinding.ActivityMainBinding
 import java.io.IOException
@@ -18,11 +19,13 @@ import java.io.IOException
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     //
-    var sensor : Sensor? = null
-    var sensorManager : SensorManager? = null
-    lateinit var display_video : VideoView
+    var sensor: Sensor? = null
+    var sensorManager: SensorManager? = null
+    lateinit var display_video: VideoView
     var mediaController: MediaController? = null
     lateinit var binding: ActivityMainBinding
+    var isRunning: Boolean = true
+    var flag = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,40 +46,43 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         display_video.requestFocus()
 
         //sensor implementation
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
-
-        //something
-
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-
-        //
-        var isRunning = false
-//        display_video.setMediaController(mediaController)
-
-        try{
-
-            //
-
-            if(event!!.values[0] < 30 && isRunning == false){ // image is visible when surrounding light is dim i.e. <30
-                isRunning = true
+        binding.toggleButton.setOnCheckedChangeListener { compoundButton, b ->
+            if(b){  //if checked
+                flag = 0
                 display_video.visibility = View.VISIBLE
                 display_video.start()
                 binding.studying.visibility = View.GONE
             }
-            else{   // image is invisible when surroundings is bright
-                isRunning = false
+            else{   //if not checked
+                flag = 1
                 display_video.visibility = View.GONE
-//                display_video.pause()
-
                 binding.studying.visibility = View.VISIBLE
             }
         }
-        catch(e : IOException){
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
 
-        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        //---------------------------
+//        isRunning = false
+            try {
+                if (event!!.values[0] < 30 && isRunning == false && flag == 0) { // image is visible when surrounding light is dim i.e. <30
+                    isRunning = true
+                    display_video.visibility = View.VISIBLE
+                    display_video.start()
+                    binding.studying.visibility = View.GONE
+                } else {   // image is invisible when surroundings is bright
+                    isRunning = false
+                    display_video.visibility = View.GONE
+                    binding.studying.visibility = View.VISIBLE
+                }
+            }
+            catch (e: IOException){
+            }
+        //--------------------------
+
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
@@ -84,13 +90,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
-//        display_video.resume()
-        sensorManager!!.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
-//        display_video.pause()
         sensorManager!!.unregisterListener(this)
     }
 }
